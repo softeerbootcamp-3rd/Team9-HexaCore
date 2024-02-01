d5f3ddd2296f67d085dbcda8d1984a34d4b40a76
timido7021 <timido7021@gmail.com>
2024-02-01 19:58:14 +0900

    feat/#5: 초기 Spring Boot 프로젝트 설정
    - Spring Boot v3.2.2
      - Java 17
      - Spring Web
      - Spring Data JPA는 DB URL이 있어야 해서 주석 처리
    - Gradle
    - AssertJ

---
+++ server/src/test/java/com/hexacore/tayo/TayoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexacore.tayo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TayoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
