89179c21bb96ef3d65a6df6b060f3b44415fd27b
thecloer <chltjrrb97@gmail.com>
2024-02-03 18:28:57 +0900

    feat(#7): 헤더 더미 컴포넌트 생성
    컴포넌트 분리 후 내용 작성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Header from '@/components/Header';
 
 function App() {
   return (
     <div>
-      <header>Header</header>
+      <Header />
       <Outlet />
     </div>
   );
