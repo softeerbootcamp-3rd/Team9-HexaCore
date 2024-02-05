4fa0fd3678b468c8869d1c22e333eaa82dab29e8
OSSU <o.ss.u2056@gmail.com>
2024-02-06 00:09:03 +0900

    feat(#20): DB 연동 확인용 User Entity 생성
    데이터베이스 연동이 잘 되었는지 확인하기 위하여 User 엔티티를 생성하여 DB에 user 테이블이 잘 생성되었는지 확인하였습니다.

---
+++ server/src/main/java/com/hexacore/tayo/user/model/UserEntity.java
@@ -0,0 +1,55 @@
+package com.hexacore.tayo.user.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "User")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "profile_img")
+    private String profileImg;
+
+    @PrePersist
+    private void onCreate() {
+        createdAt = new Date();
+        updatedAt = new Date();
+    }
+
+    @PreUpdate
+    private void onUpdate() {
+        updatedAt = new Date();
+    }
+}
