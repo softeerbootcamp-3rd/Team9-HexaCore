672fdb1f9385cb12036c4fef09dc8739acd467b8
OSSU <o.ss.u2056@gmail.com>
2024-02-05 21:09:26 +0900

    feat(#12): HttpStatus 코드 관리용 enum 타입 생성
    응답 상태코드와 status, 메시지를 관리하기 위한 enum 타입을 생성하여 기본적인 응답 값을 설정해두었습니다.

---
+++ server/src/main/java/com/hexacore/tayo/common/ResponseCode.java
@@ -0,0 +1,36 @@
+package com.hexacore.tayo.common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.util.Optional;
+import java.util.function.Predicate;
+
+@Getter
+@RequiredArgsConstructor
+public enum ResponseCode {
+    OK(200, HttpStatus.OK, "Ok"),
+    BAD_REQUEST(400, HttpStatus.BAD_REQUEST, "Bad request"),
+    UNAUTHORIZED(401, HttpStatus.UNAUTHORIZED, "Unauthorized"),
+    INTERNAL_SERVER_ERROR(500, HttpStatus.INTERNAL_SERVER_ERROR, "Internal error");
+
+    private final Integer code;
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public String getMessage(Throwable e) {
+        return this.getMessage(this.getMessage() + " - " + e.getMessage());
+    }
+
+    public String getMessage(String message) {
+        return Optional.ofNullable(message)
+                .filter(Predicate.not(String::isBlank))
+                .orElse(this.getMessage());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s (%d)", this.name(), this.getCode());
+    }
+}
