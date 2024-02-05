6dd7f99ed3836a20708d25c520695003d9992ec9
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-02-05 18:33:23 +0900

    feat(#15) : 등록된 차가 있는지 확인하기
    등록된 차가 있는 지 확인하고 없으면 등록 페이지로 redirect 시키기

--- client/src/pages/hosts/HostManage.tsx
+++ client/src/pages/hosts/HostManage.tsx
@@ -1,12 +1,24 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 function HostManage() {
+  const navigate = useNavigate();
+
+  const isVehicleRegistered = true;
+
+  useEffect(() => {
+    if (!isVehicleRegistered) {
+      navigate('/hosts/register');
+    }
+  }, [navigate, isVehicleRegistered]);
+
   return (
     <div>
       <h2>/hosts/manage</h2>
-      <p>본인이 등록한 차량의 정보를 볼 수 있고 해당 차량의 예약을 관리 할 수 있는 페이지 입니다.</p>
-      <p>차량이 등록되어 있지 않을 경우 차량을 등록 할 수 있는 페이지를 보여줍니다.</p>
+
     </div>
   );
 }
 
-export default HostManage;
 
+export default HostManage;
