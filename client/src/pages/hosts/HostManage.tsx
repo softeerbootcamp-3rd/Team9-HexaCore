cf6c9fe779905c79a79d0b8eeb83d8e1cf6b84bb
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-02-05 22:07:10 +0900

    feat(#15) : 페이지 영역 분할

--- client/src/pages/hosts/HostManage.tsx
+++ client/src/pages/hosts/HostManage.tsx
@@ -13,10 +13,18 @@ function HostManage() {
   }, [navigate, isVehicleRegistered]);
 
   return (
-    <div>
-      <h2>/hosts/manage</h2>
-
+    <div className='flex flex-row min-h-screen'>
+      <div className="w-1/2 p-6 pl-24">
+        <h2 className='font-bold text-lg'>OO 호스트님, 등록한 차량을 관리해보세요.</h2>
+        <div className='mt-6 h-2/3 rounded-3xl shadow-md	bg-white'></div>
+      </div>
+      <div className="w-1/2 p-6">
+        <h2 className='font-bold text-lg'>예약 가능 일자</h2>
+        <div className='mt-6 h-1/3 rounded-3xl shadow-md	bg-white'></div>
+        <h2 className='mt-6 font-bold text-lg'>내 차 예약 내역</h2>
+      </div>
     </div>
+
   );
 }
 
