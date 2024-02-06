f9d047c1b3790bba350aba45de8b6df05c13b1ff
OSSU <o.ss.u2056@gmail.com>
2024-02-06 20:47:27 +0900

    feat(#28): 차량 예약 가능 날짜 수정 컨트롤러 생성
    /cars/:carId/date 요청이 PUT 메소드로 들어올 경우 만들어 둔 서비스 로직을 호출하여 응답값을 반환하도록 구현하였습니다. 아직 세션 관리를 어떻게 할지 정해진 바가 없어 추후 세션 확인 관련 로직을 추가할 예정입니다.

--- server/src/main/java/com/hexacore/tayo/car/CarController.java
+++ server/src/main/java/com/hexacore/tayo/car/CarController.java
@@ -1,6 +1,13 @@
 package com.hexacore.tayo.car;
 
+import com.hexacore.tayo.car.model.DateListDto;
+import com.hexacore.tayo.common.ResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +17,10 @@ public class CarController {
 
     @Autowired
     private CarService carService;
+
+    @PutMapping("{carId}/date")
+    public ResponseEntity<ResponseDto> updateDates(@PathVariable Long carId, @RequestBody DateListDto dateListDto) {
+        return new ResponseEntity<>(carService.updateDates(carId, dateListDto), HttpStatus.OK);
+    }
 
 }
