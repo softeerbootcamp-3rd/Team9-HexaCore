b6db4ba946254353bc314f676867df46b0fa523f
OSSU <o.ss.u2056@gmail.com>
2024-02-06 14:57:16 +0900

    remove(#20): 테스트용 api 구현 제거

--- server/src/main/java/com/hexacore/tayo/car/CarController.java
+++ server/src/main/java/com/hexacore/tayo/car/CarController.java
@@ -1,12 +1,6 @@
 package com.hexacore.tayo.car;
 
-import com.hexacore.tayo.car.model.ModelDto;
-import com.hexacore.tayo.common.ResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +10,5 @@ public class CarController {
 
     @Autowired
     private CarService carService;
-
-    @PostMapping("/categories")
-    public ResponseEntity<ResponseDto> addCategories(@RequestBody ModelDto modelDto) {
-        return new ResponseEntity<>(carService.createCategory(modelDto), HttpStatus.OK);
-    }
 
 }
--- server/src/main/java/com/hexacore/tayo/car/CarService.java
+++ server/src/main/java/com/hexacore/tayo/car/CarService.java
@@ -1,9 +1,5 @@
 package com.hexacore.tayo.car;
 
-import com.hexacore.tayo.car.model.ModelDto;
-import com.hexacore.tayo.car.model.ModelEntity;
-import com.hexacore.tayo.common.ResponseCode;
-import com.hexacore.tayo.common.ResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +8,4 @@ public class CarService {
 
     @Autowired
     private ModelRepository modelRepository;
-
-    public ResponseDto createCategory(ModelDto modelDto) {
-        ModelEntity model = new ModelEntity();
-
-        model.setCategory(modelDto.getCategory());
-        model.setSubCategory(modelDto.getSubCategory());
-
-        modelRepository.save(model);
-
-        return ResponseDto.success(ResponseCode.OK, "차량 모델 등록 성공");
-    }
 }
