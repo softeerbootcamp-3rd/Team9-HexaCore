e8023ac4e477991acd2fe1d19f83c53b1f0bd323
kelly1422 <kelly1422@naver.com>
2024-02-06 11:24:46 +0900

    feat(#13): 로그인, 회원가입에 필요한 input 컴포넌트 생성
    왼쪽에 입력해야 할 내용을 적고, 오른쪽에 그 값을 입력해야 하는 input 이 오는 형식이 반복적으로 발생
    해당 컴포넌트를 생성하고 그에 맞는 프로퍼티를 받아 반복적인 코드를 줄이고자 했습니다.

---
+++ client/src/pages/auth/InputBox.tsx
@@ -0,0 +1,31 @@
+import type { ChangeEvent } from 'react';
+
+type Props = {
+	title?: string,
+	placeHolder: string;
+	type?: string;
+	value: string;
+	onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+	isWrong?: boolean;
+	className?: string;
+};
+
+function InputBox({ title, placeHolder, type = "text", value, onChange, isWrong = false, className }: Props) {
+	return (
+		<div className='w-full p-3 flex'>
+			<div className='text-background-600 w-24 text-sm pl-5 flex flex-col justify-center'>{title}</div>
+			<input className={`
+				${isWrong ? 'ring-2 ring-danger-300' : 'focus:ring-2 focus:ring-primary-300'}
+				focus:outline-none w-4/5 h-12 text-sm py-2 pl-5 shadow-md rounded-3xl
+				${className}`}
+				type={type}
+				placeholder={placeHolder}
+				value={value}
+				onChange={onChange}>
+			</input>
+		</div >
+	);
+}
+
+export default InputBox;
+
