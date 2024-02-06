5ee0df3200b0e09cec3ef8a4cf3eb629dfc39d56
OSSU <o.ss.u2056@gmail.com>
2024-02-06 11:49:35 +0900

    feat(#20): ImageEntity 생성
    차량 사진 여러개를 업로드할 수 있기 때문에 Image 테이블을 따로 분리하여 차량마다 여러개의 이미지를 갖고있을 수 있도록 설정하였습니다.

---
+++ server/src/main/java/com/hexacore/tayo/car/model/ImageEntity.java
@@ -0,0 +1,50 @@
+package com.hexacore.tayo.car.model;
+
+import com.hexacore.tayo.car.model.CarEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "Image")
+public class ImageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id", nullable = false)
+    private CarEntity car;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @Column(name = "index", nullable = false)
+    private Integer index;
+
+    @PrePersist
+    private void onCreate() {
+        createdAt = new Date();
+        updatedAt = new Date();
+    }
+
+    @PreUpdate
+    private void onUpdate() {
+        updatedAt = new Date();
+    }
+}
