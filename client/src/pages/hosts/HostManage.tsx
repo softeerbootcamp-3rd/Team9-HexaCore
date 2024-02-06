1a16d88637146f9556d2bed313bd27bb34a178a0
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-02-06 22:00:28 +0900

    fix(#14) : 응답객체를 타입으로 선언 & 데이터를 loader로 전송받음

--- client/src/pages/hosts/HostManage.tsx
+++ client/src/pages/hosts/HostManage.tsx
@@ -1,34 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import dummyData from './CarData.json';
+import { useLoaderData } from 'react-router';
 import Tag from '@/components/Tag';
 import Button from '@/components/Button';
-
-const initialDataState = {
-  success: false,
-  code: 0,
-  message: '',
-  data: {
-    car: {
-      carName: '',
-      carNumber: '',
-      imageUrls: [],
-      mileage: 0,
-      fuel: '',
-      type: '',
-      capacity: 0,
-      year: 0,
-      feePerHour: 0,
-      carAddress: '',
-      description: '',
-      dates: []
-    },
-  },
-};
+import CarData from '@/hosts/hostsRoutes';
 
 function HostManage() {
   const navigate = useNavigate();
-  const [data, setData] = useState(initialDataState);
+  const data = useLoaderData() as CarData;
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
   const handleNextImage = (index) => {
@@ -45,17 +24,10 @@ function HostManage() {
     alert("삭제가 완료되었습니다.");
     location.reload();
   };
-
-  const isVehicleRegistered = true;
-
-  useEffect(() => {
-    if (!isVehicleRegistered) {
-      navigate('/hosts/register');
-    }
-    else {
-      setData(dummyData);
-    }
-  }, [navigate, isVehicleRegistered]);
+  console.log(data)
+  if (data === null) {
+    navigate('/hosts/register');
+  }
 
   return (
     <div className='flex flex-row min-h-full'>
@@ -65,11 +37,11 @@ function HostManage() {
           <div className='w-full h-1/3 border-b border-b-gray-300 flex'>
             <div className='w-40 h-full'>
               <div className='h-3/4 w-full'>
-                <img className='w-full h-full rounded-lg' src={data.data.car.imageUrls[currentImageIndex]}></img>
+                <img className='w-full h-full rounded-lg' src={data.imageUrls[currentImageIndex]}></img>
 
               </div>
               <div className='h-1/5 flex justify-center items-center'>
-                {data.data.car.imageUrls.map((url, index) => (
+                {data.imageUrls.map((url, index) => (
                   <div
                     key={index}
                     className='cursor-pointer w-4 h-4 mx-1 bg-background-300 rounded-full'
@@ -80,31 +52,31 @@ function HostManage() {
             </div>
             <div className='flex-grow h-full pl-5 flex flex-col'>
               <div className='h-1/2'>
-                <h2 className='font-bold text-md'>모델 : {data.data.car.carName}</h2>
-                <h2 className='font-bold text-md'>차량번호 : {data.data.car.carNumber}</h2>
+                <h2 className='font-bold text-md'>모델 : {data.carName}</h2>
+                <h2 className='font-bold text-md'>차량번호 : {data.carNumber}</h2>
               </div>
 
               <div className='h-1/2 flex flex-wrap'>
-                {data.data.car.type && <Tag text={data.data.car.type}></Tag>}
-                {data.data.car.capacity && <Tag text={`${data.data.car.capacity}인승`}></Tag>}
-                {data.data.car.fuel && <Tag text={data.data.car.fuel}></Tag>}
-                {data.data.car.mileage && <Tag text={`${data.data.car.mileage}km/L`}></Tag>}
+                {data.type && <Tag text={data.type}></Tag>}
+                {data.capacity && <Tag text={`${data.capacity}인승`}></Tag>}
+                {data.fuel && <Tag text={data.fuel}></Tag>}
+                {data.mileage && <Tag text={`${data.mileage}km/L`}></Tag>}
               </div>
             </div>
           </div>
           <div className='w-full h-1/2 flex flex-col'>
-            <h2 className='font-bold text-md mt-4'>위치 : {data.data.car.carAddress}</h2>
+            <h2 className='font-bold text-md mt-4'>위치 : {data.carAddress}</h2>
             <div className='w-full h-4/5 mt-4 flex'>
               <h2 className='font-bold text-md min-w-fit'>부가설명 : </h2>
               <div className='w-4/5 bg-background-100 ml-4 rounded-lg p-6 text-sm break-words overflow-hidden'>
                 <div style={{ display: '-webkit-box', WebkitBoxOrient: 'vertical', overflow: 'hidden', WebkitLineClamp: 6 }}>
-                  {data.data.car.description}
+                  {data.description}
                 </div>
               </div>
             </div>
           </div>
           <div className='w-full h-14 flex flex-col mt-6'>
-            <h2 className='font-bold text-md text-right mb-3'>{data.data.car.feePerHour}원/ 일</h2>
+            <h2 className='font-bold text-md text-right mb-3'>{data.feePerHour}원/ 일</h2>
             <div className='flex flex-row-reverse'>
               <Button className='ml-6' text='삭제' type='danger' onClick={deleteCar}></Button>
               <Button className='ml-6' text='수정' onClick={editCar}></Button>
--- client/src/pages/hosts/hostsRoutes.tsx
+++ client/src/pages/hosts/hostsRoutes.tsx
@@ -1,10 +1,42 @@
 import type { RouteObject } from 'react-router-dom';
 import HostManage from '@/pages/hosts/HostManage';
 import HostRegister from '@/pages/hosts/HostRegister';
+import response from './CarData.json';
 
+type CarData = {
+  carName: String,
+  carNumber: String,
+  imageUrls: String[],
+  mileage: Number,
+  fuel: String,
+  type: String,
+  capacity: Number,
+  year: Number,
+  feePerHour: Number,
+  carAddress: String,
+  description: String,
+  dates: String[]
+};
 const hostsRoutes: RouteObject[] = [
   {
     path: 'hosts/manage',
+    loader: async () => {
+      const data: CarData = {
+        carName: response.data.car.carName,
+        carNumber: response.data.car.carNumber,
+        imageUrls: response.data.car.imageUrls,
+        mileage: response.data.car.mileage,
+        fuel: response.data.car.fuel,
+        type: response.data.car.type,
+        capacity: response.data.car.capacity,
+        year: response.data.car.year,
+        feePerHour: response.data.car.feePerHour,
+        carAddress: response.data.car.carAddress,
+        description: response.data.car.description,
+        dates: response.data.car.dates
+      }
+      return data;
+    },
     element: <HostManage />,
   },
   {
