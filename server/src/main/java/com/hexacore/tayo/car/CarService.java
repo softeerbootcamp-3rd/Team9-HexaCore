66deac8760015db95781dc3f5ee70438f0f30085
OSSU <o.ss.u2056@gmail.com>
2024-02-06 17:52:56 +0900

    feat(#28): 차량 예약 가능 날짜 수정 서비스로직 구현
    CarRepository에서 차량 아이디값으로 조회했을 때 해당하는 차량 정보가 없을 경우에는 NOT_FOUND 에러를 반환하도록 하였습니다. 조회되는 차량 정보가 있을 경우에는 수정된 정보로 업데이트하여 저장되도록 구현하였습니다.

--- server/src/main/java/com/hexacore/tayo/car/CarService.java
+++ server/src/main/java/com/hexacore/tayo/car/CarService.java
@@ -1,5 +1,11 @@
 package com.hexacore.tayo.car;
 
+import com.hexacore.tayo.car.model.CarEntity;
+import com.hexacore.tayo.car.model.DateListDto;
+import com.hexacore.tayo.common.ResponseCode;
+import com.hexacore.tayo.common.ResponseDto;
+import com.hexacore.tayo.common.errors.GeneralException;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,5 +13,21 @@ import org.springframework.stereotype.Service;
 public class CarService {
 
     @Autowired
-    private ModelRepository modelRepository;
+    private CarRepository carRepository;
+
+    /* 예약 가능 날짜 수정 */
+    public ResponseDto updateDates(Long carId, DateListDto dateListDto) {
+        Optional<CarEntity> car = carRepository.findById(carId);
+
+        // 차량 조회가 안 되는 경우
+        if (car.isEmpty()) {
+            throw new GeneralException(ResponseCode.NOT_FOUND, "존재하지 않는 id입니다");
+        }
+
+        // 수정된 정보로 업데이트
+        car.get().setDates(dateListDto.getDates());
+        carRepository.save(car.get());
+
+        return ResponseDto.success(ResponseCode.OK, "예약 가능 날짜 수정이 완료되었습니다");
+    }
 }
