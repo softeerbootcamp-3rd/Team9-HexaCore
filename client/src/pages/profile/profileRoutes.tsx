c9c7b2bc06b2adbca5c50cc5d452c7a29ea5518a
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-02-06 18:28:08 +0900

    feat(#31) : 마이 페이지 진입 전 데이터 로드하기

--- client/src/pages/profile/profileRoutes.tsx
+++ client/src/pages/profile/profileRoutes.tsx
@@ -1,13 +1,33 @@
 import type { RouteObject } from 'react-router-dom';
 import Profile from '@/pages/profile/Profile';
+import response from './dummy.json';
 
+type UserInfo = {
+  name: String | null,
+  nickName: String | null,
+  phoneNum: String | null,
+  email: String | null,
+  image: String | null
+}
 const profileRoutes: RouteObject[] = [
   {
     path: 'profile/:userId?',
     loader: async ({ params }) => {
       const userId = params.userId ?? 'my-userId';
-      // TODO: fetch user data
-      return userId;
+
+      if (userId !== 'my-userId') {
+        const data: UserInfo = {
+          name: response.data.name || null,
+          nickName: response.data.nickname || null,
+          email: response.data.email || null,
+          phoneNum: response.data.phoneNumber || null,
+          image: response.data.profileImg || null,
+        }
+        return data;
+      }
+      else {
+        return null;
+      }
     },
     element: <Profile />,
   },
