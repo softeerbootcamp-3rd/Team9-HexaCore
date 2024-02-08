c1886757283213b8797ca582be148d6f9e239607
OSSU <o.ss.u2056@gmail.com>
2024-02-08 11:32:04 +0900

    feat(#40): 기존 서비스 로직 및 컨트롤러에 ErrorCode 반영
    ResponseCode를 사용하던 부분들을 HttpStatus나 ErrorCode가 전달되도록 수정하였습니다.

--- server/src/main/java/com/hexacore/tayo/car/CarController.java
+++ server/src/main/java/com/hexacore/tayo/car/CarController.java
@@ -1,9 +1,9 @@
 package com.hexacore.tayo.car;
 
 import com.hexacore.tayo.car.model.DateListDto;
-import com.hexacore.tayo.common.ResponseCode;
 import com.hexacore.tayo.common.ResponseDto;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,15 +12,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/cars")
 public class CarController {
 
-    @Autowired
-    private CarService carService;
+    private final CarService carService;
 
     @PutMapping("{carId}/date")
     public ResponseEntity<ResponseDto> updateDates(@PathVariable Long carId, @RequestBody DateListDto dateListDto) {
         ResponseDto responseDto = carService.updateDates(carId, dateListDto);
-        return new ResponseEntity<>(responseDto, ResponseCode.valueOf(responseDto.getCode()));
+        return new ResponseEntity<>(responseDto, HttpStatusCode.valueOf(responseDto.getCode()));
     }
 }
--- server/src/main/java/com/hexacore/tayo/car/CarService.java
+++ server/src/main/java/com/hexacore/tayo/car/CarService.java
@@ -2,10 +2,11 @@ package com.hexacore.tayo.car;
 
 import com.hexacore.tayo.car.model.CarEntity;
 import com.hexacore.tayo.car.model.DateListDto;
-import com.hexacore.tayo.common.ResponseCode;
 import com.hexacore.tayo.common.ResponseDto;
+import com.hexacore.tayo.common.errors.ErrorCode;
 import com.hexacore.tayo.common.errors.GeneralException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,11 +19,11 @@ public class CarService {
     public ResponseDto updateDates(Long carId, DateListDto dateListDto) {
         CarEntity car = carRepository.findById(carId)
                 // 차량 조회가 안 되는 경우
-                .orElseThrow(() -> new GeneralException(ResponseCode.NOT_FOUND, "존재하지 않는 id입니다"));
+                .orElseThrow(() -> new GeneralException(ErrorCode.CAR_NOT_FOUND));
 
         car.setDates(dateListDto.getDates());
         carRepository.save(car);
 
-        return ResponseDto.success(ResponseCode.OK, "예약 가능 날짜 수정이 완료되었습니다");
+        return ResponseDto.success(HttpStatus.ACCEPTED);
     }
 }
