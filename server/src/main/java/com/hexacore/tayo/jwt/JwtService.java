45bcb79d989338f3ea645f78bf5d18b09469bacd
kelly1422 <kelly1422@naver.com>
2024-02-08 11:00:23 +0900

    feat(#37): jwt를 서명으로 검증하고 그 안에 저장된 값을 불러오는 메소드 작성

--- server/src/main/java/com/hexacore/tayo/jwt/JwtService.java
+++ server/src/main/java/com/hexacore/tayo/jwt/JwtService.java
@@ -1,10 +1,13 @@
 package com.hexacore.tayo.jwt;
 
+import com.hexacore.tayo.common.errors.AuthException;
+import com.hexacore.tayo.common.errors.ErrorCode;
 import com.hexacore.tayo.user.model.UserEntity;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -40,7 +43,7 @@ public class JwtService {
 
     public String createRefreshToken(String userId, Long id) {
         String refreshToken = Jwts.builder()
-                .setSubject(userId)
+                .setSubject(userId) // 토큰을 발급한 주체
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + refreshTokenExpirationPeriod))
                 .signWith(key, SignatureAlgorithm.HS256)
@@ -49,5 +52,22 @@ public class JwtService {
         // todo 리프레시 토큰 디비에 저장
 
         return refreshToken;
+    }
+
+    /**
+     * @param token
+     * @return 서명을 이용해 토큰을 검증하고 토큰을 파싱해 토큰에 저장된 값을 읽어온다.
+     */
+    public Claims getClaims(String token) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(key) // 서명을 검증할 키
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (SignatureException e) {
+            // 서명 검증에 실패한 경우
+            throw new AuthException(ErrorCode.INVALID_JWT_TOKEN);
+        }
     }
 }
