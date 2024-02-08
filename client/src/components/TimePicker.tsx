090f007dc8dfaf6e535d389fa4ddc5838b4da47a
OSSU <o.ss.u2056@gmail.com>
2024-02-08 10:17:19 +0900

    style(#28): 탭 을 공백으로 변환

--- client/src/components/TimePicker.tsx
+++ client/src/components/TimePicker.tsx
@@ -1,36 +1,36 @@
 import React, { useState } from 'react';
 
 type Props = {
-	onTimeChange: (time: string) => void;
-	id: string;
-	className?: string;
+  onTimeChange: (time: string) => void;
+  id: string;
+  className?: string;
 };
 
 function TimePicker({ onTimeChange, id, className }: Props) {
-	const [selectedHour, setSelectedHour] = useState('12');
+  const [selectedHour, setSelectedHour] = useState('12');
 
-	const handleHourChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-		setSelectedHour(event.target.value);
-		onTimeChange(event.target.value);
-	};
+  const handleHourChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedHour(event.target.value);
+    onTimeChange(event.target.value);
+  };
 
-	return (
-		<select
-			className={`
-				text-center
-				${className}
-			`}
-			id={id}
-			value={selectedHour}
-			onChange={handleHourChange} >
-			{
-				Array.from({ length: 24 }, (_, i) => {
-					const hour = (i + 1).toString().padStart(2, '0');
-					return <option key={hour} value={hour}>{hour}</option>;
-				})
-			}
-		</select >
-	);
+  return (
+    <select
+      className={`
+        text-center
+        ${className}
+      `}
+      id={id}
+      value={selectedHour}
+      onChange={handleHourChange} >
+      {
+        Array.from({ length: 24 }, (_, i) => {
+          const hour = (i + 1).toString().padStart(2, '0');
+          return <option key={hour} value={hour}>{hour}</option>;
+        })
+      }
+    </select >
+  );
 }
 
 export default TimePicker;
