a08d74a9bfc48d3356cd85264bdf7e353a789232
kelly1422 <kelly1422@naver.com>
2024-02-10 22:04:42 +0900

    feat(#37): 리프레시 토큰을 확인하는 RefreshAccessTokenInterceptor 작성
    해당 인터셉터에서 리프레시 토큰의 유효성을 검증하고 이후 엑세스 토큰을 재발급 받는 컨트롤러로 넘어가도록 구현하였습니다.

---
+++ server/src/main/java/com/hexacore/tayo/interceptor/RefreshAccessTokenInterceptor.java
@@ -0,0 +1,44 @@
+package com.hexacore.tayo.interceptor;
+
+import com.hexacore.tayo.common.errors.AuthException;
+import com.hexacore.tayo.common.errors.ErrorCode;
+import com.hexacore.tayo.jwt.JwtService;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Date;
+
+@RequiredArgsConstructor
+public class RefreshAccessTokenInterceptor implements HandlerInterceptor {
+    private final JwtService jwtService;
+
+    @Value("${jwt.refresh.cookie-name}")
+    private String refreshTokenCookieName;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String clientRefreshToken = RequestParser.getToken(request, refreshTokenCookieName);
+        Claims clientClaims = jwtService.getClaims(clientRefreshToken);
+
+        // 리프레시 토큰 만료 여부를 확인
+        if (clientClaims.getExpiration().before(new Date())) {
+            throw new AuthException(ErrorCode.EXPIRED_REFRESH_TOKEN);
+        }
+
+        String serverRefreshToken = jwtService.getRefreshToken(Long.valueOf(clientClaims.get("userId", String.class)));
+        // 디비에 저장되어 있는 토큰의 유효성 검증
+        jwtService.checkTokenValidation(serverRefreshToken);
+
+        // 클라이언트로부터 받은 리프레시 토큰과, 디비에 저장해 놓은 리프레시 토큰 값이 같은지 비교
+        if (clientRefreshToken.equals(serverRefreshToken)) {
+            return true;
+        } else {
+            throw new AuthException(ErrorCode.INVALID_JWT_TOKEN);
+        }
+    }
+}
