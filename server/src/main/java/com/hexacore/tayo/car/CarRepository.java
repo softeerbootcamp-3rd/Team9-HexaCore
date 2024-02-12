8c4060e0c04f67dcc9bab1cd0671b58a3f4e7b50
thecloer <chltjrrb97@gmail.com>
2024-02-12 22:55:23 +0900

    feat(#55): Specifications으로 동적 검색 쿼리 구현, 날짜 검색은 보류
    날짜 저장 형식에 대한 논의가 필요함

--- server/src/main/java/com/hexacore/tayo/car/CarRepository.java
+++ server/src/main/java/com/hexacore/tayo/car/CarRepository.java
@@ -4,15 +4,15 @@ import com.hexacore.tayo.car.model.CarEntity;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarRepository extends JpaRepository<CarEntity, Long> {
+public interface CarRepository extends JpaRepository<CarEntity, Long>, JpaSpecificationExecutor<CarEntity> {
 
     Optional<CarEntity> findByOwner_IdAndCarNumberAndIsDeletedTrue(Long ownerId, String carNumber);
 
     List<CarEntity> findByOwner_IdAndIsDeletedFalse(Long ownerId);
-
 
     List<CarEntity> findByCarNumberAndIsDeletedFalse(String carNumber);
 }
