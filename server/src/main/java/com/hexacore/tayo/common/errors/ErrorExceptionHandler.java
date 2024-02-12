a8e477c3e9dd482b743047e25022b04062be6a5e
OSSU <o.ss.u2056@gmail.com>
2024-02-12 22:09:53 +0900

    refactor(#72): 수정된 공통 응답 형식 ErrorExceptionHandler에 적용

--- server/src/main/java/com/hexacore/tayo/common/errors/ErrorExceptionHandler.java
+++ server/src/main/java/com/hexacore/tayo/common/errors/ErrorExceptionHandler.java
@@ -1,8 +1,9 @@
 package com.hexacore.tayo.common.errors;
 
-import com.hexacore.tayo.common.ResponseDto;
-import org.springframework.http.HttpStatus;
+import com.hexacore.tayo.common.CommonResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,14 +11,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ErrorExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ResponseDto> handleException(Exception e) {
-        return new ResponseEntity<>(ResponseDto.error(ErrorCode.SERVER_ERROR, e),
-                HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<CommonResponse> handleException() {
+        return CommonResponse.error(ErrorCode.SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CommonResponse> handleValidationExceptions(MethodArgumentNotValidException e) {
+        BindingResult result = e.getBindingResult();
+        String message = result.getFieldErrors().get(0).getDefaultMessage();
+        return CommonResponse.error(ErrorCode.VALIDATION_ERROR, message);
     }
 
     @ExceptionHandler(GeneralException.class)
-    public ResponseEntity<ResponseDto> handleGeneralException(GeneralException e) {
-        return new ResponseEntity<>(ResponseDto.error(e.getErrorCode(), e),
-                e.getErrorCode().getHttpStatus());
+    public ResponseEntity<CommonResponse> handleGeneralException(GeneralException e) {
+        return CommonResponse.error(e.getErrorCode());
     }
 }
