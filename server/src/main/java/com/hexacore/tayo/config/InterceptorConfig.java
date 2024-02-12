9719eb44f07bb6b76c14723faf1bca99d443a5dc
OSSU <o.ss.u2056@gmail.com>
2024-02-13 06:24:52 +0900

    feat(#66): loggingAspect가 인터셉터 중 가장 먼저 수행되도록 config 수정

--- server/src/main/java/com/hexacore/tayo/config/InterceptorConfig.java
+++ server/src/main/java/com/hexacore/tayo/config/InterceptorConfig.java
@@ -3,6 +3,7 @@ package com.hexacore.tayo.config;
 import com.hexacore.tayo.auth.interceptor.AuthenticationInterceptor;
 import com.hexacore.tayo.auth.interceptor.RefreshAccessTokenInterceptor;
 import com.hexacore.tayo.auth.JwtService;
+import com.hexacore.tayo.log.LoggingAspect;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,14 +17,18 @@ public class InterceptorConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        // 요청에 대한 로깅
+        registry.addInterceptor(loggingAspect())
+                .order(1);
+
         registry.addInterceptor(authenticationInterceptor())
-                .order(1)
+                .order(2)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/signup", "/login", "/refresh"); // 로그인 인증 필요없는 요청
 
         // 엑세스 토큰 재발급 요청인 경우
         registry.addInterceptor(refreshAccessTokenInterceptor())
-                .order(2)
+                .order(3)
                 .addPathPatterns("/refresh");
     }
 
@@ -35,5 +40,10 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Bean
     public AuthenticationInterceptor authenticationInterceptor() {
         return new AuthenticationInterceptor(jwtService);
+    }
+
+    @Bean
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
     }
 }
