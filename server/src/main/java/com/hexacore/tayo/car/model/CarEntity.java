c3a9ba898d5acc04b74e5c15decb500ca6802031
thecloer <chltjrrb97@gmail.com>
2024-02-12 17:30:05 +0900

    feat(#55): car와 image 일대다 양방향 매핑
    자동차 정보를 불러오는 경우 항상 이미지도 함께 불러오므로 양방향 매핑

--- server/src/main/java/com/hexacore/tayo/car/model/CarEntity.java
+++ server/src/main/java/com/hexacore/tayo/car/model/CarEntity.java
@@ -2,15 +2,9 @@ package com.hexacore.tayo.car.model;
 
 import com.hexacore.tayo.common.BaseTime;
 import com.hexacore.tayo.user.model.UserEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -82,4 +76,7 @@ public class CarEntity extends BaseTime {
     @Column(name = "is_deleted", nullable = false, columnDefinition = "boolean default false")
     @Builder.Default
     private Boolean isDeleted = false;
+
+    @OneToMany(mappedBy = "image")
+    private List<ImageEntity> images = new ArrayList<>();
 }
