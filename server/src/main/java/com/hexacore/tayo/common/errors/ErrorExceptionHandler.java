ec386db4288cac9048aa04fb417ee2b14eb88f42
OSSU <o.ss.u2056@gmail.com>
2024-02-13 21:49:25 +0900

    rename(#72): ErrorExceptionHandler에 통합한 Response 반영

--- server/src/main/java/com/hexacore/tayo/common/errors/ErrorExceptionHandler.java
+++ server/src/main/java/com/hexacore/tayo/common/errors/ErrorExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.hexacore.tayo.common.errors;
 
-import com.hexacore.tayo.common.response.ResponseDto;
-import org.springframework.http.HttpStatus;
+import com.hexacore.tayo.common.response.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,22 +11,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ErrorExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ResponseDto> handleException(Exception e) {
-        return new ResponseEntity<>(ResponseDto.error(ErrorCode.SERVER_ERROR, e),
-                HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<Response> handleException(Exception e) {
+        return Response.of(ErrorCode.SERVER_ERROR);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ResponseDto> handleValidationExceptions(MethodArgumentNotValidException e) {
+    public ResponseEntity<Response> handleValidationExceptions(MethodArgumentNotValidException e) {
         BindingResult result = e.getBindingResult();
         String message = result.getFieldErrors().get(0).getDefaultMessage();
-        return new ResponseEntity<>(ResponseDto.error(ErrorCode.VALIDATION_ERROR, message),
-                HttpStatus.BAD_REQUEST);
+        return Response.of(ErrorCode.VALIDATION_ERROR, message);
     }
 
     @ExceptionHandler(GeneralException.class)
-    public ResponseEntity<ResponseDto> handleGeneralException(GeneralException e) {
-        return new ResponseEntity<>(ResponseDto.error(e.getErrorCode(), e),
-                e.getErrorCode().getHttpStatus());
+    public ResponseEntity<Response> handleGeneralException(GeneralException e) {
+        return Response.of(e.getErrorCode());
     }
 }
