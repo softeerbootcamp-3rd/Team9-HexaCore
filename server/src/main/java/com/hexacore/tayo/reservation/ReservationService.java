1af75b6a62b520246e27d046e1cef0fbf9d8bb74
OSSU <o.ss.u2056@gmail.com>
2024-02-13 21:48:44 +0900

    rename(#72): Reservation 관련 서비스 및 컨트롤러에 통합한 Response 반영

--- server/src/main/java/com/hexacore/tayo/reservation/ReservationService.java
+++ server/src/main/java/com/hexacore/tayo/reservation/ReservationService.java
@@ -7,8 +7,6 @@ import com.hexacore.tayo.car.model.CarImage;
 import com.hexacore.tayo.category.model.SubCategory;
 import com.hexacore.tayo.common.errors.ErrorCode;
 import com.hexacore.tayo.common.errors.GeneralException;
-import com.hexacore.tayo.common.response.DataResponseDto;
-import com.hexacore.tayo.common.response.ResponseDto;
 import com.hexacore.tayo.reservation.dto.CreateReservationRequestDto;
 import com.hexacore.tayo.reservation.dto.GetGuestReservationListResponseDto;
 import com.hexacore.tayo.reservation.dto.GetHostReservationListResponseDto;
@@ -24,7 +22,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -36,7 +33,7 @@ public class ReservationService {
     private final CarImageRepository imageRepository;
 
     @Transactional
-    public ResponseDto createReservation(CreateReservationRequestDto createReservationRequestDto) {
+    public void createReservation(CreateReservationRequestDto createReservationRequestDto) {
         long guestUserId = 13L; // TODO: JWT 토큰에서 userId를 추출하고 로그인한 상태에서만 실행되도록 추가
         // 현재 예시는 Guest 유저의 ID:13
 
@@ -69,11 +66,9 @@ public class ReservationService {
                 .status(ReservationStatus.READY)
                 .build();
         reservationRepository.save(reservation);
-
-        return ResponseDto.success(HttpStatus.OK);
     }
 
-    public ResponseDto getGuestReservations() {
+    public GetGuestReservationListResponseDto getGuestReservations() {
         // TODO: JWT 토큰에서 userId를 추출하고 로그인한 상태에서만 실행되도록 추가
         long guestUserId = 13L;
 
@@ -106,10 +101,10 @@ public class ReservationService {
             getGuestReservationResponseDtos.add(getGuestReservationResponseDto);
         }
 
-        return DataResponseDto.of(new GetGuestReservationListResponseDto(getGuestReservationResponseDtos));
+        return new GetGuestReservationListResponseDto(getGuestReservationResponseDtos);
     }
 
-    public ResponseDto getHostReservations() {
+    public GetHostReservationListResponseDto getHostReservations() {
         // TODO: JWT 토큰에서 userId를 추출하고 로그인한 상태에서만 실행되도록 추가
         long hostUserId = 1L;
 
@@ -138,17 +133,15 @@ public class ReservationService {
             getHostReservationResponseDtos.add(getHostReservationResponseDto);
         }
 
-        return DataResponseDto.of(new GetHostReservationListResponseDto(getHostReservationResponseDtos));
+        return new GetHostReservationListResponseDto(getHostReservationResponseDtos);
     }
 
     @Transactional
-    public ResponseDto cancelReservation(Long reservationId) {
+    public void cancelReservation(Long reservationId) {
         Reservation reservation = reservationRepository.findById(reservationId)
                 .orElseThrow(() -> new GeneralException(ErrorCode.RESERVATION_NOT_FOUND));
 
         reservation.setStatus(ReservationStatus.CANCEL);
         reservationRepository.save(reservation);
-
-        return ResponseDto.success(HttpStatus.OK);
     }
 }
