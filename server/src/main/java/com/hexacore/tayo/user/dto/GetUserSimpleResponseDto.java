024926496e0cc0fe51071ff393e9f7701a484106
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-02-13 17:05:33 +0900

    Merge branch 'feat/#78' into dev

--- server/src/main/java/com/hexacore/tayo/user/dto/GetUserSimpleResponseDto.java
+++ server/src/main/java/com/hexacore/tayo/user/dto/GetUserSimpleResponseDto.java
@@ -1,10 +1,6 @@
 package com.hexacore.tayo.user.dto;
 
-<<<<<<<< HEAD:server/src/main/java/com/hexacore/tayo/user/dto/GetUserSimpleResponseDto.java
 import com.hexacore.tayo.user.model.User;
-========
-import com.hexacore.tayo.user.model.UserEntity;
->>>>>>>> dev:server/src/main/java/com/hexacore/tayo/user/dto/UserSimpleDto.java
 import lombok.Getter;
 
 @Getter
