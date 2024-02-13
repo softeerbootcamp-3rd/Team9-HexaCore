929dbd454140b19232eb9f8b1b01988d67b83305
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-02-14 01:10:11 +0900

    feat(#90): 유저 프로필에 기본값 설정하기

--- server/src/main/java/com/hexacore/tayo/user/dto/GetUserSimpleResponseDto.java
+++ server/src/main/java/com/hexacore/tayo/user/dto/GetUserSimpleResponseDto.java
@@ -1,6 +1,7 @@
 package com.hexacore.tayo.user.dto;
 
 import com.hexacore.tayo.user.model.User;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,10 +16,12 @@ public class GetUserSimpleResponseDto {
     private String profileImgUrl;
     private String phoneNumber;
 
+    @SuppressWarnings("checkstyle:NeedBraces")
     public GetUserSimpleResponseDto(User user) {
         this.id = user.getId();
         this.name = user.getName();
-        this.profileImgUrl = user.getProfileImgUrl();
+        this.profileImgUrl = (user.getProfileImgUrl() != null) ? user.getProfileImgUrl()
+                : "https://hexacore-bucket.s3.ap-northeast-2.amazonaws.com/defaultProfile.png";
         this.phoneNumber = user.getPhoneNumber();
     }
 
