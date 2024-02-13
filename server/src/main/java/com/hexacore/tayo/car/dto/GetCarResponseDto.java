e50cd9838f0d307e5776156642c20a8090e25447
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-02-14 01:09:42 +0900

    feat(#90): 타입을 한글로 반환하기 & dates null일때 빈 리스트 반환하기

--- server/src/main/java/com/hexacore/tayo/car/dto/GetCarResponseDto.java
+++ server/src/main/java/com/hexacore/tayo/car/dto/GetCarResponseDto.java
@@ -1,28 +1,52 @@
 package com.hexacore.tayo.car.dto;
 
 import com.hexacore.tayo.car.model.Car;
-import com.hexacore.tayo.car.model.CarType;
 import com.hexacore.tayo.user.dto.GetUserSimpleResponseDto;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import lombok.Getter;
 
 @Getter
 public class GetCarResponseDto {
 
-    private GetUserSimpleResponseDto host;
-    private String carName;
-    private String carNumber;
-    private List<String> imageUrls;
-    private Double mileage;
-    private String fuel;
-    private CarType type;
-    private Integer capacity;
-    private Integer year;
-    private Integer feePerHour;
-    private String address;
-    private String description;
-    private List<List<LocalDateTime>> dates;
+    @NotNull
+    private final GetUserSimpleResponseDto host;
+
+    @NotNull
+    private final String carName;
+
+    @NotNull
+    private final String carNumber;
+
+    @NotNull
+    private final List<String> imageUrls;
+
+    @NotNull
+    private final Double mileage;
+
+    @NotNull
+    private final String fuel;
+
+    @NotNull
+    private final String type;
+
+    @NotNull
+    private final Integer capacity;
+
+    @NotNull
+    private final Integer year;
+
+    @NotNull
+    private final Integer feePerHour;
+
+    @NotNull
+    private final String address;
+
+    private final String description;
+    private final List<List<LocalDateTime>> dates = new ArrayList<>();
 
     public GetCarResponseDto(Car car, List<String> images) {
         this.carName = car.getSubCategory().getName();
@@ -30,13 +54,13 @@ public class GetCarResponseDto {
         this.imageUrls = images;
         this.mileage = car.getMileage();
         this.fuel = car.getFuel();
-        this.type = car.getType();
+        this.type = car.getType().getType();
         this.capacity = car.getCapacity();
         this.year = car.getYear();
         this.feePerHour = car.getFeePerHour();
         this.address = car.getAddress();
         this.description = car.getDescription();
-        this.dates = car.getDates();
+        Optional.ofNullable(car.getDates()).ifPresent(this.dates::addAll);
         this.host = new GetUserSimpleResponseDto(car.getOwner());
     }
 }
