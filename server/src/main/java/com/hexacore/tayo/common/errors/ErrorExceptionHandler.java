1a0dc2e63391a26e790cfc610546136c75306a3c
OSSU <o.ss.u2056@gmail.com>
2024-02-13 12:04:21 +0900

    refactor(#72): HTTP 응답 형태 수정에 따른 ErrorExceptionHandler 수정

--- server/src/main/java/com/hexacore/tayo/common/errors/ErrorExceptionHandler.java
+++ server/src/main/java/com/hexacore/tayo/common/errors/ErrorExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.hexacore.tayo.common.errors;
 
-import com.hexacore.tayo.common.CommonResponse;
+import com.hexacore.tayo.common.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,19 +11,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ErrorExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<CommonResponse> handleException() {
-        return CommonResponse.error(ErrorCode.SERVER_ERROR);
+    public ResponseEntity<Response> handleException() {
+        return Response.of(ErrorCode.SERVER_ERROR);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<CommonResponse> handleValidationExceptions(MethodArgumentNotValidException e) {
+    public ResponseEntity<Response> handleValidationExceptions(MethodArgumentNotValidException e) {
         BindingResult result = e.getBindingResult();
         String message = result.getFieldErrors().get(0).getDefaultMessage();
-        return CommonResponse.error(ErrorCode.VALIDATION_ERROR, message);
+        return Response.of(ErrorCode.VALIDATION_ERROR, message);
     }
 
     @ExceptionHandler(GeneralException.class)
-    public ResponseEntity<CommonResponse> handleGeneralException(GeneralException e) {
-        return CommonResponse.error(e.getErrorCode());
+    public ResponseEntity<Response> handleGeneralException(GeneralException e) {
+        return Response.of(e.getErrorCode());
     }
 }
