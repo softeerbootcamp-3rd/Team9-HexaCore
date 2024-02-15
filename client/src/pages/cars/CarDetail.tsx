0f919294b1baa01ad650dfea55ae647c1f1c0304
OSSU <o.ss.u2056@gmail.com>
2024-02-15 17:09:30 +0900

    feat(#98): 차량 상세 정보 페이지 달력 연결
    달력 연결을 위해 날짜와 시간으로 분리하여 시작 날짜, 대여 시간, 반납 날짜, 반납 시간을 관리하고 사용자가 시간을 업데이트 할 때 대여 DateTime이 반납 DateTime보다 뒤에 있는 경우 업데이트가 안 되도록 수정하였습니다.

--- client/src/pages/cars/CarDetail.tsx
+++ client/src/pages/cars/CarDetail.tsx
@@ -1,56 +1,59 @@
-import { useLoaderData, useNavigate } from 'react-router-dom';
+import { Navigate, useLoaderData, useNavigate } from 'react-router-dom';
 import { CarData } from './carRoutes';
 import { useEffect, useState } from 'react';
 import Tag from '@/components/Tag';
 import Button from '@/components/Button';
 import TimePicker from '@/components/TimePicker';
+import GuestCalendar from '@/components/calendar/guestCalendar/GuestCalendar';
+import { generateDateRange } from '@/components/calendar/guestCalendar/calendar.guest';
+import { DateRange } from '@/components/calendar/calendar.core';
+import { stringTupleToDateRange } from '@/utils/converters';
+import { server } from '@/fetches/common/axios';
+import { ResponseWithoutData } from '@/fetches/common/response.type';
+import ImageGallery from './ImageGallery';
 
 function CarDetail() {
-  const navigator = useNavigate();
+  const navigate = useNavigate();
   const data = useLoaderData() as CarData;
-  const [currentIdx, setCurrentIdx] = useState(0);
+
+  const [rentTime, setRentTime] = useState(9);
+  const [returnTime, setReturnTime] = useState(9);
+  
   const [totalFee, setTotalFee] = useState(0);
 
-  const [startDateTime, setStartDateTime] = useState(new Date(2024, 1, 2, 12, 0, 0, 0));
-  const [endDateTime, setEndDateTime] = useState(new Date(2024, 1, 3, 12, 0, 0, 0));
-
-  // 다음 이미지 표시
-  const showNextImage = () => {
-    setCurrentIdx((prevIdx) => (prevIdx + 1) % data.imageUrls.length);
+  // DateTime을 string으로 변환
+  const dateTimeToString = (dateTime: Date) => {
+    const date = dateTimeToDate(dateTime);
+    const hours = dateTime.getHours().toString().padStart(2, '0');
+    const time = `${hours}:00:00`
+    return `${date}T${time}`;
   };
 
-  // 이전 이미지 표시
-  const showPrevImage = () => {
-    setCurrentIdx((prevIdx) => (prevIdx - 1 + data.imageUrls.length) % data.imageUrls.length);
+  // DateTime에서 Date parsing
+  const dateTimeToDate = (date: Date) => {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
   };
 
-  // 대여 시간 정보 업데이트
-  const updateStartDateTime = (time: string) => {
-    setStartDateTime((prevDate: Date) => {
-      const newDate = new Date(prevDate);
-      newDate.setHours(parseInt(time));
+  const params = new URLSearchParams(window.location.search);
+  const startDate = params.get('startDate') || dateTimeToDate(new Date());
+  const endDate = params.get('endDate') || dateTimeToDate(new Date());
 
-      return newDate;
-    });
-  };
-
-  // 반납 시간 정보 업데이트
-  const updateEndDateTime = (time: string) => {
-    setEndDateTime((prevDate: Date) => {
-      const newDate = new Date(prevDate);
-      newDate.setHours(parseInt(time));
-
-      return newDate;
-    });
-  };
+  const [dateRange, setDateRange] = useState<DateRange>(stringTupleToDateRange([startDate, endDate]));
 
   // 가격 계산 함수
-  const calculatePrice = () => {
-    console.log('endDateTime: ' + endDateTime);
-    console.log('startDateTime: ' + startDateTime);
+  const calculatePrice = (dateRange : DateRange) => {
+    const startDate = new Date(dateRange[0]);
+    const endDate = new Date(dateRange[1]);
+
+    // 시간 정보 업데이트
+    startDate.setHours(rentTime);
+    endDate.setHours(returnTime);
 
     // 시간 간격 계산 (밀리초 단위)
-    const timeDiff = endDateTime.getTime() - startDateTime.getTime();
+    const timeDiff = endDate.getTime() - startDate.getTime();
 
     // 시간 간격을 시간 단위로 변환
     const hourInterval = timeDiff / (1000 * 60 * 60);
@@ -62,49 +65,102 @@ function CarDetail() {
     setTotalFee(calculatedFee);
   };
 
-  // TimePicker 값 변경 시 가격 재계산
-  useEffect(() => {
-    calculatePrice();
-  }, [startDateTime, endDateTime]);
+  // 대여 시간 정보 업데이트
+  const updateStartDateTime = (time: string) => {
+    setRentTime((prevTime: number) => {
+      const newTime = parseInt(time);
 
-  // DateTime을 string으로 변환
-  const dateTimeToString = (date: Date) => {
-    const year = date.getFullYear();
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
-    const day = date.getDate().toString().padStart(2, '0');
-    return `${year}-${month}-${day}T${date.getHours()}:00:00`;
+      const startDateTime = new Date(dateRange[0]);
+      startDateTime.setHours(newTime);
+
+      const endDateTime = new Date(dateRange[1]);
+      endDateTime.setHours(returnTime);
+
+      if(startDateTime > endDateTime) {
+        return prevTime;
+      }
+
+      return newTime;
+    });
   };
 
+  // 반납 시간 정보 업데이트
+  const updateEndDateTime = (time: string) => {
+    setReturnTime((prevTime: number) => {
+      const newTime = parseInt(time);
+
+      const startDateTime = new Date(dateRange[0]);
+      startDateTime.setHours(rentTime);
+
+      const endDateTime = new Date(dateRange[1]);
+      endDateTime.setHours(newTime);
+
+      if(startDateTime > endDateTime) {
+        return prevTime;
+      }
+
+      return newTime;
+    });
+  };
+
+  // 예약 날짜 정보 업데이트
+  const updateDateRange = (dateRange : DateRange) => {
+    setDateRange(() => {
+      const newDateRange = dateRange;
+      newDateRange[0].setHours(rentTime);
+      newDateRange[1].setHours(returnTime);
+
+      return newDateRange;
+    })
+  }
+
+  // TimePicker 값 변경 시 가격 재계산
+  useEffect(() => {
+    calculatePrice(dateRange);
+  }, [updateStartDateTime, updateEndDateTime]);
+
+  if(data === null) {
+     return <Navigate to=".."/>
+  }
+
   // 예약하기 버튼 클릭 핸들러
-  const handleReservation = () => {
+  const handleReservation = async () => {
+    const startDate = new Date(dateRange[0]);
+    const endDate = new Date(dateRange[1]);
+
+    // 시간 정보 업데이트
+    startDate.setHours(rentTime);
+    endDate.setHours(returnTime);
+
     // date를 string으로 변환
-    const rentDate = dateTimeToString(startDateTime);
-    const returnDate = dateTimeToString(endDateTime);
+    const rentDate = dateTimeToString(startDate);
+    const returnDate = dateTimeToString(endDate);
 
     const reservationData = {
       carId: data.carId,
       rentDate: rentDate,
       returnDate: returnDate,
     };
+
     console.log(reservationData);
 
-    // 마이페이지로 이동
-    navigator('/profile/1');
+    // 예약하기 api 호출
+    const response = await server.post<ResponseWithoutData>('/reservations', {
+      data: reservationData
+    });
+
+    if(response.success) {
+      // 마이페이지로 이동
+      navigate('/profile/1');
+    } else {
+      alert(response.message);
+    }
   };
 
   return (
     <div className="flex flex-col gap-8 px-40">
       {/* Image Gallery */}
-      <div className="relative mx-10 flex h-[500px] overflow-hidden rounded-lg">
-        <button onClick={showPrevImage} className="absolute left-5 top-1/2 -translate-y-1/2 transform hover:brightness-75">
-          <img src="/prev-button.svg" alt="Prev Button Image" />
-        </button>
-        <img className="w-full rounded-lg object-cover" src={data.imageUrls[currentIdx]} alt="car-img" />
-        <button onClick={showNextImage} className="absolute right-5 top-1/2 -translate-y-1/2 transform hover:brightness-75">
-          <img src="/next-button.svg" alt="Next Button Image" />
-        </button>
-      </div>
-
+      <ImageGallery imageUrls={data.imageUrls} className="mx-10 h-[500px] rounded-xl" />
       {/* Car Detail + Reservation */}
       <div className="mx-10 mb-10 flex gap-9">
         {/* Car Detail */}
@@ -116,7 +172,7 @@ function CarDetail() {
               <Tag className="h-6" text={`${data.capacity}인승`} />
             </div>
             <p className="text-background-500">
-              차종 {data.type} &#183; 연비 {data.mileage}km/l &#183; 연료 {data.fuel}
+              차종 {data.type} &#183; 연비 {data.mileage}km/L &#183; 연료 {data.fuel}
             </p>
           </div>
 
@@ -125,8 +181,8 @@ function CarDetail() {
 
           {/* Host Info */}
           <div className="flex items-center gap-4">
-            <div className="bg-gray-300 flex h-14 w-14 items-center justify-center overflow-hidden rounded-full">
-              <img src={data.host.profileImg} alt="host-profile" />
+            <div className="bg-gray-300 flex h-10 w-10 items-center justify-center overflow-hidden rounded-full">
+              <img src={data.host.profileImg || '/default-profile.svg'} alt="host-profile" />
             </div>
             <div className="flex flex-col">
               <p className="font-semibold">호스트</p>
@@ -141,8 +197,8 @@ function CarDetail() {
           <div className="flex flex-col gap-3">
             {/* carNumber */}
             <div className="flex items-center gap-4">
-              <div className="bg-gray-300 flex h-14 w-14 items-center justify-center overflow-hidden rounded-full">
-                <img src={data.host.profileImg} alt="host-profile" />
+              <div className="flex h-10 w-10 items-center justify-center pl-3 overflow-hidden rounded-full">
+                <img src='/truck.svg' alt="car-number" />
               </div>
               <div className="flex flex-col">
                 <p className="font-semibold">차량 번호</p>
@@ -152,8 +208,8 @@ function CarDetail() {
 
             {/* Year */}
             <div className="flex items-center gap-4">
-              <div className="bg-gray-300 flex h-14 w-14 items-center justify-center overflow-hidden rounded-full">
-                <img src={data.host.profileImg} alt="host-profile" />
+              <div className="flex h-10 w-10 items-center justify-center pl-3 overflow-hidden rounded-full">
+                <img src='/year.svg' alt="year" />
               </div>
               <div className="flex flex-col">
                 <p className="font-semibold">연식</p>
@@ -163,12 +219,12 @@ function CarDetail() {
 
             {/* Address */}
             <div className="flex items-center gap-4">
-              <div className="bg-gray-300 flex h-14 w-14 items-center justify-center overflow-hidden rounded-full">
-                <img src={data.host.profileImg} alt="host-profile" />
+              <div className="flex h-10 w-10 items-center justify-center pl-3 overflow-hidden rounded-full">
+                <img src='/location.svg' alt="car-address" />
               </div>
               <div className="flex flex-col">
                 <p className="font-semibold">픽업 위치</p>
-                <p className="text-sm text-background-500">{data.carAddress}</p>
+                <p className="text-sm text-background-500">{data.address}</p>
               </div>
             </div>
           </div>
@@ -186,28 +242,33 @@ function CarDetail() {
         <div className="flex h-fit w-2/5 flex-col justify-center gap-4 rounded-3xl bg-white p-6 shadow-xl">
           {/* Total Fee */}
           <p className="text-gray-900 text-3xl tracking-tight">₩ {totalFee}</p>
-          <div>캘린더</div>
+          <div>
+            <GuestCalendar 
+              availableDates={[generateDateRange({ from: new Date(), years: 5 })]}
+              onReservationChange={updateDateRange}
+              reservation={dateRange}/>
+          </div>
           {/* Date + Time Info */}
           <div className="grid grid-cols-2 gap-0 overflow-hidden rounded-xl border-[1px] border-background-300">
             <label className="flex flex-col gap-1 border-b-[0.5px] border-r-[0.5px] border-background-300 p-3" htmlFor="rentHourSelect">
               <p className="text-xs font-medium">대여일</p>
               <p className="text-background-500">
-                {startDateTime.getFullYear()}-{(startDateTime.getMonth() + 1).toString().padStart(2, '0')}-{startDateTime.getDate().toString().padStart(2, '0')}
+                {dateTimeToDate(dateRange[0])}
               </p>
             </label>
             <label className="flex flex-col gap-1 border-b-[0.5px] border-l-[0.5px] border-background-300 p-3" htmlFor="rentHourSelect">
               <p className="text-xs font-medium">반납일</p>
               <p className="text-background-500">
-                {endDateTime.getFullYear()}-{(endDateTime.getMonth() + 1).toString().padStart(2, '0')}-{endDateTime.getDate().toString().padStart(2, '0')}
+                {dateTimeToDate(dateRange[1])}
               </p>
             </label>
             <div className="gap-1 border-r-[0.5px] border-t-[0.5px] border-background-300 p-3">
               <p className="text-xs font-medium">대여 시각</p>
-              <TimePicker className="w-full text-background-500" id={'rentHourSelect'} onTimeChange={updateStartDateTime} />
+              <TimePicker className="w-full text-background-500" id={'rentHourSelect'} onTimeChange={updateStartDateTime} time={rentTime.toString().padStart(2, '0')} />
             </div>
             <div className="gap-1 border-l-[0.5px] border-t-[0.5px] border-background-300 p-3">
               <p className="text-xs font-medium">반납 시각</p>
-              <TimePicker className="w-full text-background-500" id={'returnHourSelect'} onTimeChange={updateEndDateTime} />
+              <TimePicker className="w-full text-background-500" id={'returnHourSelect'} onTimeChange={updateEndDateTime} time={returnTime.toString().padStart(2, '0')} />
             </div>
           </div>
 
@@ -220,4 +281,3 @@ function CarDetail() {
 }
 
 export default CarDetail;
-
