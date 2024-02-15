3344239a9e3ee2a8a019d767125073d76e6a394c
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-02-15 10:23:02 +0900

    test(#97): 주석 제거

--- server/src/test/java/com/hexacore/tayo/car/CarControllerTest.java
+++ server/src/test/java/com/hexacore/tayo/car/CarControllerTest.java
@@ -20,7 +20,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 @AutoConfigureMockMvc
