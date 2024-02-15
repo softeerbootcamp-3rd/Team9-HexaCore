ec523a024980246d5d56aa381566184bfd932218
OSSU <o.ss.u2056@gmail.com>
2024-02-15 15:27:00 +0900

    feat(#98): 차량 관련 응답 타입 선언
    CarDetailData : 프론트에서 필요한 차량 세부 정보 타입
    CarDetailJsonData : 서버에서 응답값으로 받은 차량 세부 정보 타입

---
+++ client/src/fetches/cars/cars.type.ts
@@ -0,0 +1,38 @@
+import { DateRange } from '@/components/calendar/calendar.core';
+
+export type HostData = {
+	name: string;
+	profileImg: string;
+}
+
+export type CarDetailData = {
+  carName: string;
+  carNumber: string;
+  imageUrls: string[];
+  mileage: number;
+  fuel: string;
+  type: string;
+  capacity: number;
+  year: number;
+  feePerHour: number;
+  address: string;
+  description: string;
+  carDateRanges: DateRange[];
+	host: HostData;
+};
+
+export type CarDetailJsonData = {
+  carName: string;
+  carNumber: string;
+  imageUrls: string[];
+  mileage: number;
+  fuel: string;
+  type: string;
+  capacity: number;
+  year: number;
+  feePerHour: number;
+  address: string;
+  description: string;
+  carDateRanges: string[][];
+	host: HostData;
+}
