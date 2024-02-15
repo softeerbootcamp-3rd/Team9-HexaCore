9a3974b5ef52c41c8a93f992a413f99393e70e82
thecloer <chltjrrb97@gmail.com>
2024-02-15 16:01:39 +0900

    chore(#55): 라이브러리 정리

--- server/src/main/java/com/hexacore/tayo/car/CarController.java
+++ server/src/main/java/com/hexacore/tayo/car/CarController.java
@@ -20,6 +20,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/cars")
