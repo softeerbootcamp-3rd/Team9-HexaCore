e17b4765cd58ceece78d029c0ff11155b4862dc1
Yoon-Suji <70956926+Yoon-Suji@users.noreply.github.com>
2024-02-16 11:24:53 +0900

    refactor(#24): 사용하지 않는 주석 제거

--- client/src/fetches/cars/fetchCars.ts
+++ client/src/fetches/cars/fetchCars.ts
@@ -1,15 +1,12 @@
 import { server } from '@/fetches/common/axios';
 import { ResponseWithPagination } from '@/fetches/common/response.type';
 import type { CarData, CarSearchParam } from '@/fetches/cars/cars.type';
-// import responseCar from '@/pages/home/dummy/cars.json';
 
 export const PAGE_SORT = 'id,DESC';
 export const PAGE_SIZE = 20;
 export const DISTANCE = 5000;
 
 export const fetchCars = async ({ lat, lng, startDate, endDate, party, type, category, subcategory, minPrice, maxPrice, page, size, sort }: CarSearchParam) => {
-  // 더미 데이터 테스트 용
-  // return {data: responseCar.data, pageInfo: responseCar.pageInfo};
   const response = await server.get<ResponseWithPagination<CarData[]>>('/cars', {
     params: {
       lat: lat,
