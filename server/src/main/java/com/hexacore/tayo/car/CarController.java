c32d15986155940c972978a5d1ccc367230ae135
thecloer <chltjrrb97@gmail.com>
2024-02-18 21:34:50 +0900

    feat(#137): 검색 요청 파라미터 받는 방식 RequestParam에서 ModelAttribute로 변경, 유효성 검사 추가

--- server/src/main/java/com/hexacore/tayo/car/CarController.java
+++ server/src/main/java/com/hexacore/tayo/car/CarController.java
@@ -2,25 +2,21 @@ package com.hexacore.tayo.car;
 
 import com.hexacore.tayo.car.dto.CreateCarRequestDto;
 import com.hexacore.tayo.car.dto.GetCarResponseDto;
+import com.hexacore.tayo.car.dto.SearchCarsDto;
+import com.hexacore.tayo.car.dto.SearchCarsRequestDto;
+import com.hexacore.tayo.car.dto.SearchCarsResultDto;
 import com.hexacore.tayo.car.dto.UpdateCarDateRangeRequestDto.CarDateRangesDto;
 import com.hexacore.tayo.car.dto.UpdateCarRequestDto;
-import com.hexacore.tayo.car.dto.*;
 import com.hexacore.tayo.car.model.Car;
-import com.hexacore.tayo.car.model.CarType;
-import com.hexacore.tayo.car.dto.SearchCarsParamsDto;
-import com.hexacore.tayo.common.Position;
 import com.hexacore.tayo.common.response.Response;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,33 +27,11 @@ public class CarController {
 
     @GetMapping()
     public ResponseEntity<Response> getCars(
-        @RequestParam Double distance,
-        @RequestParam Double lat,
-        @RequestParam Double lng,
-        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDate startDate,
-        @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDate endDate,
-        @RequestParam Integer party,
-        @RequestParam(required = false) String type,
-        @RequestParam(required = false) Integer categoryId,
-        @RequestParam(required = false) Integer subcategoryId,
-        @RequestParam(required = false) Integer minPrice,
-        @RequestParam(required = false) Integer maxPrice,
+        @Valid @ModelAttribute SearchCarsRequestDto searchCarsRequestDto,
         Pageable pageable
     ) {
-        SearchCarsParamsDto searchCarsParamsDto = SearchCarsParamsDto.builder()
-                .distance(distance)
-                .position(new Position(lat, lng))
-                .startDate(startDate)
-                .endDate(endDate)
-                .party(party)
-                .type(CarType.of(type))
-                .categoryId(categoryId)
-                .subcategoryId(subcategoryId)
-                .minPrice(minPrice)
-                .maxPrice(maxPrice)
-                .build();
-
-        Page<Car> cars = carService.searchCars(searchCarsParamsDto, pageable);
+        SearchCarsDto searchCarsDto = SearchCarsDto.of(searchCarsRequestDto);
+        Page<Car> cars = carService.searchCars(searchCarsDto, pageable);
         Page<SearchCarsResultDto> data = cars.map(SearchCarsResultDto::of);
         return Response.of(HttpStatus.OK, data);
     }
