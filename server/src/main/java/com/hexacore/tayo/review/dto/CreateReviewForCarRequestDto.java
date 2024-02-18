f56cd4568aad2a7adf058911c8ae5546ec996b11
OSSU <o.ss.u2056@gmail.com>
2024-02-19 00:19:28 +0900

    feat(#164): 차량에 대한 리뷰 정보를 받아올 dto 생성
    차량에 대한 리뷰를 작성할 경우 reservationId, 리뷰 내용, 별점 정보를 포함하도록 설정하였습니다.

---
+++ server/src/main/java/com/hexacore/tayo/review/dto/CreateReviewForCarRequestDto.java
@@ -0,0 +1,22 @@
+package com.hexacore.tayo.review.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.hibernate.validator.constraints.Range;
+
+@Getter
+@AllArgsConstructor
+public class CreateReviewForCarRequestDto {
+
+    @NotNull(message = "reservationId가 null 이어서는 안됩니다.")
+    private Long reservationId;
+
+    @NotBlank(message = "리뷰 내용이 null 이어서는 안됩니다.")
+    private String contents;
+
+    @NotNull(message = "별점이 null 이어서는 안됩니다.")
+    @Range(max = 5, message = "별점은 0 ~ 5 사이의 정수값이어야 합니다.")
+    private Integer rate;
+}
