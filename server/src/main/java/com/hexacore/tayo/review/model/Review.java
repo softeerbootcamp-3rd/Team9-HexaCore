c166b7e02e9fb91781ff08557996ba8cb95a3e26
OSSU <o.ss.u2056@gmail.com>
2024-02-18 20:33:59 +0900

    feat(#164): 리뷰 엔티티 생성
    리뷰의 경우 게스트가 차량을 리뷰하는 것과 호스트가 게스트를 리뷰하는 2가지 경우에 대해 공통적인 필드값을 공유하게 되므로 FK를 작성자(writer) / 게스트(guest) or 차 (car) 중 하나와 연결되도록 설정해두었습니다.

---
+++ server/src/main/java/com/hexacore/tayo/review/model/Review.java
@@ -0,0 +1,40 @@
+package com.hexacore.tayo.review.model;
+
+import com.hexacore.tayo.car.model.Car;
+import com.hexacore.tayo.common.BaseTime;
+import com.hexacore.tayo.user.model.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "review")
+public class Review extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "writer_id", nullable = false)
+    private User writer;
+
+    @ManyToOne
+    @JoinColumn(name = "guest_id")
+    private User guest;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id")
+    private Car car;
+
+    @Column(name = "contents")
+    private String contents;
+
+    @Column(name = "rate")
+    private Integer rate;
+}
