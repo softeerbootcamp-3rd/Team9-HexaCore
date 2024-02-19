e87c6fcdab76ef70bac90efb2f222c2f5505ce12
kelly1422 <kelly1422@naver.com>
2024-02-20 02:51:28 +0900

    feat(#201): SseEmitter 저장소

---
+++ server/src/main/java/com/hexacore/tayo/notification/sse/SseEmitterRepository.java
@@ -0,0 +1,43 @@
+package com.hexacore.tayo.notification.sse;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class SseEmitterRepository {
+
+    // 모든 Emitter 를 저장하는 ConcurrentHashMap
+    private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+    /**
+     * 주어진 아이디와 Emitter 저장
+     *
+     * @param id      - 사용자 아이디
+     * @param emitter - 이벤트 Emitter
+     */
+    public void save(Long id, SseEmitter emitter) {
+        emitters.put(id, emitter);
+    }
+
+    /**
+     * 주어진 아이디의 Emitter 를 제거
+     *
+     * @param id - 사용자 아이디.
+     */
+    public void deleteById(Long id) {
+        emitters.remove(id);
+    }
+
+    /**
+     * 주어진 아이디의 Emitter 를 가져옴.
+     *
+     * @param id 사용자 아이디.
+     * @return 이벤트 Emitter.
+     */
+    public SseEmitter get(Long id) {
+        return emitters.get(id);
+    }
+}
