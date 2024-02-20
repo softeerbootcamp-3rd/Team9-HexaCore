cc1176a87a01468ad77164a4d1b8382ba270305e
kelly1422 <kelly1422@naver.com>
2024-02-20 17:49:37 +0900

    feat(#213): 각 알림을 보여줄 컴포넌트 작성

---
+++ client/src/components/notification/NotificationItem.tsx
@@ -0,0 +1,37 @@
+import { NotificationData } from "@/fetches/notification/notification.type";
+import CloseIcon from "../svgs/CloseIcon";
+
+type Props = {
+  item: NotificationData
+};
+
+function NotificationItem({item}: Props) {
+  return (
+    <div className='flex flex-row justify-between border-t border-background-200 px-5 py-[19px]'>
+
+      <div className='flex flex-col'>
+        <div className='flex flex-row items-center'>
+          <div className={
+            `${item.title === '예약 완료' ? 'bg-primary-400' : 'bg-danger-400'} 
+            rounded-full w-2 h-2 mr-3`}>
+          </div>
+          <h2 className='font-semibold text-background-600 text-[15px]'>
+            {item.title}
+          </h2>
+        </div>
+
+        <p className='mt-1 leading-6 text-background-500 text-[14px]'>
+          {item.message}
+        </p>
+      </div>
+
+      <div>
+        <CloseIcon />
+      </div>
+
+    </div>
+  );
+}
+  
+export default NotificationItem;
+  
