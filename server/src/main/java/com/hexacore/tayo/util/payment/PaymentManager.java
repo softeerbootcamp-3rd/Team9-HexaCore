ed85861bb847a9de904c0c83bc3b7f2e38f61b27
Yoon-Suji <jidl88@ewhain.net>
2024-02-20 18:35:28 +0900

    feat(#192): POST /reservation API 수정
    - TossPayments와 통신하는 로직 util로 이동
    - Reservation 테이블에 paymentKey 추가

--- server/src/main/java/com/hexacore/tayo/util/payment/PaymentManager.java
+++ server/src/main/java/com/hexacore/tayo/util/payment/PaymentManager.java
@@ -4,7 +4,10 @@ import com.hexacore.tayo.common.errors.ErrorCode;
 import com.hexacore.tayo.common.errors.GeneralException;
 import com.hexacore.tayo.util.payment.TossPaymentDto.TossBilling;
 import com.hexacore.tayo.util.payment.TossPaymentDto.TossBillingRequest;
+import com.hexacore.tayo.util.payment.TossPaymentDto.TossConfirmRequest;
+import com.hexacore.tayo.util.payment.TossPaymentDto.TossPaymentResponse;
 import java.util.Base64;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -45,6 +48,33 @@ public class PaymentManager {
 
             if (response.getBody() == null || !response.getStatusCode().equals(HttpStatus.OK)) {
                 // 빌링 키 발급 실패
+                throw new GeneralException(ErrorCode.TOSS_PAYMENTS_FAILED);
+            }
+            return response.getBody();
+        } catch (Exception e) {
+            throw new GeneralException(ErrorCode.TOSS_PAYMENTS_FAILED, e.getMessage());
+        }
+    }
+
+    /* 빌링 결제(자동 결제) 요청 */
+    public TossPaymentResponse confirmBilling(Integer amount, String orderName, String customerKey, String billingKey) {
+        String encodedCredentials = getEncodedCredentials();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + encodedCredentials);
+
+        String orderId = UUID.randomUUID().toString();
+        HttpEntity<TossConfirmRequest> requestEntity = new HttpEntity<>(
+                TossConfirmRequest.builder().amount(amount).customerKey(customerKey).orderId(orderId).orderName(orderName).build(), headers);
+        try {
+            ResponseEntity<TossPaymentResponse> response = restTemplate.exchange(
+                    "https://api.tosspayments.com/v1/billing/" + billingKey,
+                    HttpMethod.POST,
+                    requestEntity,
+                    TossPaymentResponse.class
+            );
+
+            if (response.getBody() == null || !"DONE".equals(response.getBody().getStatus())) {
+                // 결제 승인 실패
                 throw new GeneralException(ErrorCode.TOSS_PAYMENTS_FAILED);
             }
             return response.getBody();
--- server/src/main/java/com/hexacore/tayo/util/payment/TossPaymentDto.java
+++ server/src/main/java/com/hexacore/tayo/util/payment/TossPaymentDto.java
@@ -1,5 +1,6 @@
 package com.hexacore.tayo.util.payment;
 
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,6 +11,16 @@ public class TossPaymentDto {
         private String customerKey;
         private String authKey;
     }
+
+    @Getter
+    @Builder
+    public static class TossConfirmRequest {
+        private Integer amount;
+        private String customerKey;
+        private String orderId;
+        private String orderName;
+    }
+
     @Getter
     public static class TossBilling {
         private String mid;
@@ -27,5 +38,36 @@ public class TossPaymentDto {
         private String number;
         private String cardType;
         private String ownerType;
+    }
+
+    @Getter
+    public static class TossPaymentResponse {
+        private String version;
+        private String paymentKey;
+        private String type;
+        private String orderId;
+        private String orderName;
+        private String mid;
+        private String currency;
+        private String method;
+        private Integer totalAmount;
+        private Integer balanceAmount;
+        private String status;
+        private String requestedAt;
+        private String approvedAt;
+        private List<TossCancel> cancels;
+    }
+
+    @Getter
+    private static class TossCancel {
+        private int cancelAmount;
+        private String cancelReason;
+        private int taxFreeAmount;
+        private int taxExemptionAmount;
+        private int refundableAmount;
+        private int easyPayDiscountAmount;
+        private String canceledAt;
+        private String transactionKey;
+        private String receiptKey;
     }
 }
