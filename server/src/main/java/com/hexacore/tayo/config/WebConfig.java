4ca55ea1a70bb0b456044c5da3fb2e4e6c4b6d7b
kelly1422 <kelly1422@naver.com>
2024-02-21 13:27:52 +0900

    feat(#213): 불필요한 커밋 제거

--- server/src/main/java/com/hexacore/tayo/config/WebConfig.java
+++ server/src/main/java/com/hexacore/tayo/config/WebConfig.java
@@ -3,12 +3,10 @@ package com.hexacore.tayo.config;
 import com.hexacore.tayo.common.UriPath;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
