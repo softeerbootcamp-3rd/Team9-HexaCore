bdd0fd89cf43a112a85d482793111e63e0509577
OSSU <o.ss.u2056@gmail.com>
2024-02-21 23:43:23 +0900

    feat(#164): 경고창에 info 값으로 색상 구분하도록 추가

--- client/src/components/Toast.tsx
+++ client/src/components/Toast.tsx
@@ -3,10 +3,11 @@ import { useState, useEffect } from 'react';
 type ToastProps = {
   title: string;
   message: string;
+  info: boolean;
   duration?: number;
 };
 
-function Toast({ title, message, duration = 2000 }: ToastProps) {
+function Toast({ title, message, info = false, duration = 2000 }: ToastProps) {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
@@ -20,21 +21,20 @@ function Toast({ title, message, duration = 2000 }: ToastProps) {
 
   return isVisible && title && message ? (
     <div className={`fixed bottom-0 mb-4 flex w-full justify-center transition-opacity duration-300 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
-      <div className='rounded-lg bg-danger-500 px-4 py-2 text-white shadow'>
-        <div className='flex'>
-          <div className='flex flex-col justify-center text-lg font-bold'>{title}</div>
-          <button
-            onClick={() => setIsVisible(false)}
-            type='button'
-            className='ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent p-1 text-sm text-white'
-            data-modal-hide='default-modal'>
-            <svg className='h-3 w-3' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 14 14'>
-              <path stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6' />
-            </svg>
-          </button>
+      <div className={`flex gap-4 justify-between rounded-lg px-4 py-2 text-white shadow ${info ? 'bg-info-400' : 'bg-danger-500'}`}>
+        <div className='flex flex-col'>
+          <div className='justify-center text-base font-bold'>{title}</div>
+          <div className='text-sm'>{message}</div>
         </div>
-
-        <div>{message}</div>
+        <button
+          onClick={() => setIsVisible(false)}
+          type='button'
+          className='inline-flex h-8 w-8 items-center justify-end rounded-lg bg-transparent text-sm text-white'
+          data-modal-hide='default-modal'>
+          <svg className='h-3 w-3' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 14 14'>
+            <path stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6' />
+          </svg>
+        </button>
       </div>
     </div>
   ) : null;
@@ -44,17 +44,19 @@ export const useCustomToast = () => {
   const [message, setMessage] = useState('');
   const [title, setTitle] = useState('');
   const [key, setKey] = useState(0);
+  const [info, setInfo] = useState(false);
   const [duration, setDuration] = useState<number | undefined>(2000);
 
-  const showToast = (title: string, newMessage: string, duration?: number) => {
+  const showToast = (title: string, newMessage: string, info?: boolean, duration?: number) => {
     setTitle(title);
     setMessage(newMessage);
     setKey((prevKey) => prevKey + 1);
     setDuration(duration);
+    info && setInfo(info);
   };
 
   return {
-    ToastComponent: () => <Toast key={key} title={title} message={message} duration={duration} />,
+    ToastComponent: () => <Toast key={key} title={title} message={message} duration={duration} info={info} />,
     showToast,
   };
 };
