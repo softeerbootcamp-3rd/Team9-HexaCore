c1cbcf84e153d1a70c56462a95dfe4effb5fa492
김지훈 <87347699+jomulagy@users.noreply.github.com>
2024-02-22 15:57:24 +0900

    chore(#151): 변수로 변경

--- server/src/main/java/com/hexacore/tayo/car/CarService.java
+++ server/src/main/java/com/hexacore/tayo/car/CarService.java
@@ -351,15 +351,15 @@ public class CarService {
 
     private List<List<String>> getCarAvailableDatesForGuest(Car car) {
         List<List<String>> result = new ArrayList<>();
-        //가능일이 없으면 종료
-        if (car.getCarDateRanges().isEmpty()) {
-            return result;
-        }
-
         List<CarDateRange> carAvailableDates = car.getCarDateRanges().stream()
                 .filter(carDateRange -> carDateRange.getEndDate().isAfter(LocalDate.now()))
                 .sorted(Comparator.comparing(CarDateRange::getStartDate))
                 .toList();
+
+        //가능일이 없으면 종료
+        if (carAvailableDates.isEmpty()) {
+            return result;
+        }
         //status가 CANCEL이 아닌 예약 리스트를 시작날짜의 오름 차순으로 정렬
         List<Reservation> sortedReservations = car.getReservations().stream()
                 .filter((reservation -> reservation.getStatus() != ReservationStatus.CANCEL))
