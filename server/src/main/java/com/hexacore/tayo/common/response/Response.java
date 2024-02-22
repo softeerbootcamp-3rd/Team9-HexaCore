908ed3b21323b4e93bd09547188d475fa9add78f
OSSU <o.ss.u2056@gmail.com>
2024-02-22 14:27:54 +0900

    feat(#164): 페이지 인포에 totlaElements 추가

--- server/src/main/java/com/hexacore/tayo/common/response/Response.java
+++ server/src/main/java/com/hexacore/tayo/common/response/Response.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.hexacore.tayo.common.errors.ErrorCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -51,11 +52,20 @@ public class Response {
         );
     }
 
+    public static ResponseEntity<Response> of(HttpStatus status, Page<?> page) {
+        return new ResponseEntity<>(
+                new Response(true, status.value(), status.getReasonPhrase(),
+                        new PageInfo(page.getNumber(), page.getSize(), page.hasNext(), page.getTotalElements()),
+                        page.getContent()),
+                status
+        );
+    }
+
     // 페이지네이션에 대한 응답
     public static ResponseEntity<Response> of(HttpStatus status, Slice<?> page) {
         return new ResponseEntity<>(
                 new Response(true, status.value(), status.getReasonPhrase(),
-                        new PageInfo(page.getNumber(), page.getSize(), page.hasNext()), page.getContent()),
+                        new PageInfo(page.getNumber(), page.getSize(), page.hasNext(), null), page.getContent()),
                 status
         );
     }
