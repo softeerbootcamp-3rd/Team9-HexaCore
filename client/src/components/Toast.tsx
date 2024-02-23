92385c5cb00566c4dffb23946fa487aaafafad92
OSSU <o.ss.u2056@gmail.com>
2024-02-23 18:32:15 +0900

    fix(#248): stroke 관련 property 이름 수정

--- client/src/components/Toast.tsx
+++ client/src/components/Toast.tsx
@@ -32,7 +32,7 @@ function Toast({ title, message, info = false, duration = 2000, isVisible, setIs
           className='inline-flex h-8 w-8 items-center justify-end rounded-lg bg-transparent text-sm text-white'
           data-modal-hide='default-modal'>
           <svg className='h-3 w-3' aria-hidden='true' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 14 14'>
-            <path stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6' />
+            <path stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6' />
           </svg>
         </button>
       </div>
