f02f8a10a2bbbb34c5fea56d2f2968ef0c3ef475
Yoon-Suji <jidl88@ewhain.net>
2024-02-24 23:47:50 +0900

    fix(#248): 서버와 연결이 끊어져도 메인 페이지 렌더링 되도록 수정

--- client/src/fetches/categories/fetchCategories.ts
+++ client/src/fetches/categories/fetchCategories.ts
@@ -3,11 +3,14 @@ import { ResponseWithData } from '@/fetches/common/response.type';
 import type { CategoryResponse } from '@/fetches/categories/categories.type';
 
 export const fetchCategories = async (): Promise<CategoryResponse[]> => {
-  const response = await server.get<ResponseWithData<CategoryResponse[]>>('/categories');
-  if (!response || !response.success) {
-    // TODO: response.success가 false인 경우 처리
+  try {
+    const response = await server.get<ResponseWithData<CategoryResponse[]>>('/categories');
+    if (!response || !response.success) {
+      return [];
+    }
+    return response.data;
+  } catch (e) {
     return [];
   }
-  return response.data;
 };
 
